db0792e08373a2e3b2cef16a69ba4d56dd9ce0cd
VargaJohanna <varga.johanna@gmail.com>
2020-02-02 20:29:26 +0000

    Give joke to jokelibrary and show the new activity

--- jokelibrary/src/main/java/com/android/jokelibrary/JokeActivity.java
+++ jokelibrary/src/main/java/com/android/jokelibrary/JokeActivity.java
@@ -2,13 +2,28 @@ package com.android.jokelibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class JokeActivity extends AppCompatActivity {
+    public static String JOKE_EXTRA = "JOKE_EXTRA";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Bundle extras = getIntent().getExtras();
+        String jokeToShow = "";
+        if(extras != null) {
+            jokeToShow = extras.getString(JOKE_EXTRA);
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joke);
+
+        setJoke(jokeToShow);
+    }
+
+    private void setJoke(String joke) {
+        TextView jokeText = findViewById(R.id.tv_joke);
+        jokeText.setText(joke);
     }
 }
