6a1c2efc5ccbc1c9a644571f7c7d2c40415bb7dd
VargaJohanna <varga.johanna@gmail.com>
2020-02-10 21:58:53 +0000

    Show interstitial ad

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -2,6 +2,7 @@ package com.udacity.gradle.builditbigger;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,9 +12,13 @@ import android.widget.ProgressBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.jokelibrary.JokeActivity;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
 
 public class MainActivity extends AppCompatActivity implements OnTaskCompleted {
     ProgressBar progressBar = null;
+    private InterstitialAd mInterstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,25 +27,30 @@ public class MainActivity extends AppCompatActivity implements OnTaskCompleted {
 
         Button jokeButton = findViewById(R.id.btn_joke);
         progressBar = findViewById(R.id.progressBar);
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                startAsyncTask();
+            }
+
+        });
         tellJoke(jokeButton);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
@@ -52,10 +62,22 @@ public class MainActivity extends AppCompatActivity implements OnTaskCompleted {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                progressBar.setVisibility(View.VISIBLE);
-                new EndpointsAsyncTask(MainActivity.this).execute();
+                if(getResources().getBoolean(R.bool.showAd)) {
+                    if (mInterstitialAd.isLoaded()) {
+                        mInterstitialAd.show();
+                    } else {
+                        Log.d("TAG", "The interstitial wasn't loaded yet.");
+                    }
+                } else {
+                    startAsyncTask();
+                }
             }
         });
+    }
+
+    private void startAsyncTask() {
+        progressBar.setVisibility(View.VISIBLE);
+        new EndpointsAsyncTask(MainActivity.this).execute();
     }
 
     @Override
